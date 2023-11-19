620571a9ce5fd7210b16649729faed4f39dadf6d
Anupam Sahu <sahuanupam7@gmail.com>
2023-11-20 00:01:12 +0530

    refactored some ifs and elses

--- Source/EditorExtensions/Private/RpGraphVisualizer.cpp
+++ Source/EditorExtensions/Private/RpGraphVisualizer.cpp
@@ -10,76 +10,64 @@
 IMPLEMENT_HIT_PROXY(HGraphVisProxy, HComponentVisProxy);
 IMPLEMENT_HIT_PROXY(HNodeVisProxy, HGraphVisProxy)
 
-FRpGraphVisualizer::FRpGraphVisualizer()
-{
-	FirstSelectedIndex = INDEX_NONE;
-}
-
 void FRpGraphVisualizer::DrawVisualization(const UActorComponent* Component, const FSceneView* View, FPrimitiveDrawInterface* PDI)
 {
-	SpatialGraphComponent = Cast<URpSpatialGraphComponent>(Component);
-	if(!SpatialGraphComponent)
+	GraphComponent = const_cast<URpSpatialGraphComponent*>(Cast<URpSpatialGraphComponent>(Component));
+	if(!GraphComponent)
 	{
 		return;
 	}
 	
-	const int32 NumNodes = SpatialGraphComponent->GetNumNodes();
+	const int32 NumNodes = GraphComponent->GetNumNodes();
 	for(int Index = 0; Index < NumNodes; ++Index)
 	{
-		const FVector& NodeLocation = SpatialGraphComponent->GetNodeLocation(Index);
+		const FVector& NodeLocation = GraphComponent->GetNodeLocation(Index);
 			
-		FLinearColor Color = (Index == FirstSelectedIndex || Index == SecondSelectedIndex) ? FLinearColor::White : FLinearColor::Gray;
+		FLinearColor Color = (Index == FirstSelectedIndex || Index == SecondSelectedIndex) ? FLinearColor::White : FLinearColor::Black;
 		PDI->SetHitProxy(new HNodeVisProxy(Component, Index));
 		PDI->DrawPoint(NodeLocation, Color, 20.0f, SDPG_Foreground);
 		PDI->SetHitProxy(nullptr);
 
-		TSet<uint32> Connections = SpatialGraphComponent->GetConnections(Index);
+		TSet<uint32> Connections = GraphComponent->GetConnections(Index);
 		for(const uint32 Connection : Connections)
 		{
 			// Note deletion and DrawLine might be called asynchronously causing an exception where supplied index is invalid.
-			if(!SpatialGraphComponent->IsValidIndex(Connection))
+			if(!GraphComponent->IsValidIndex(Connection))
 			{
 				continue;
 			}
-			PDI->DrawLine(NodeLocation, SpatialGraphComponent->GetNodeLocation(Connection), FLinearColor::Blue, SDPG_Foreground, 1.0f);
+			PDI->DrawLine(NodeLocation, GraphComponent->GetNodeLocation(Connection), FLinearColor::White, SDPG_Foreground, 2.0f);
 		}
 	}
 }
 
 bool FRpGraphVisualizer::VisProxyHandleClick(FEditorViewportClient* InViewportClient, HComponentVisProxy* VisProxy, const FViewportClick& Click)
 {
-	bool bEditing = false;
-
-	if(VisProxy && VisProxy->Component.IsValid())
-	{
-		bEditing = true;
-		if(VisProxy->IsA(HNodeVisProxy::StaticGetType()))
-		{
-			HNodeVisProxy* Proxy = static_cast<HNodeVisProxy*>(VisProxy);
-			if(InViewportClient->IsCtrlPressed() && FirstSelectedIndex != INDEX_NONE)
-			{
-				SecondSelectedIndex = Proxy->SelectedIndex;
-			}
-			else
-			{
-				FirstSelectedIndex = Proxy->SelectedIndex;
-			}
-		}
-	}
-	else
+	if(!VisProxy || !VisProxy->Component.IsValid() || !VisProxy->IsA(HNodeVisProxy::StaticGetType()))
 	{
 		SecondSelectedIndex = INDEX_NONE;
 		FirstSelectedIndex = INDEX_NONE;
+		return false;
+	}
+		
+	HNodeVisProxy* Proxy = static_cast<HNodeVisProxy*>(VisProxy);
+	if(InViewportClient->IsCtrlPressed() && FirstSelectedIndex != INDEX_NONE)
+	{
+		SecondSelectedIndex = Proxy->SelectedIndex;
+	}
+	else
+	{
+		FirstSelectedIndex = Proxy->SelectedIndex;
 	}
 
-	return bEditing;
+	return true;
 }
 
 bool FRpGraphVisualizer::GetWidgetLocation(const FEditorViewportClient* ViewportClient, FVector& OutLocation) const
 {
-	if(IsValid(SpatialGraphComponent) && FirstSelectedIndex != INDEX_NONE)
+	if(IsValid(GraphComponent) && FirstSelectedIndex != INDEX_NONE)
 	{
-		OutLocation = SpatialGraphComponent->GetNodeLocation(FirstSelectedIndex);
+		OutLocation = GraphComponent->GetNodeLocation(FirstSelectedIndex);
 		return true;
 	}
 
@@ -88,26 +76,27 @@ bool FRpGraphVisualizer::GetWidgetLocation(const FEditorViewportClient* Viewport
 
 bool FRpGraphVisualizer::HandleInputDelta(FEditorViewportClient* ViewportClient, FViewport* Viewport, FVector& DeltaTranslate, FRotator& DeltaRotate, FVector& DeltaScale)
 {
-	bool bHandled = false;
+	if(!IsValid(GraphComponent))
+	{
+		return false;
+	}
 
 	if(ViewportClient->IsAltPressed() && bAllowDuplication)
 	{
 		bAllowDuplication = false;
-		const FVector& NewLocation = SpatialGraphComponent->GetNodeLocation(FirstSelectedIndex) + DeltaTranslate;
+		const FVector& NewLocation = GraphComponent->GetNodeLocation(FirstSelectedIndex) + DeltaTranslate;
 
 		const int32 PreviouslySelectedIndex = FirstSelectedIndex;
-		FirstSelectedIndex = const_cast<URpSpatialGraphComponent*>(SpatialGraphComponent)->AddNode(NewLocation);
-		const_cast<URpSpatialGraphComponent*>(SpatialGraphComponent)->ConnectNodes(FirstSelectedIndex, PreviouslySelectedIndex);
+		FirstSelectedIndex = GraphComponent->AddNode(NewLocation);
+		GraphComponent->ConnectNodes(FirstSelectedIndex, PreviouslySelectedIndex);
 	}
-	
-	if(IsValid(SpatialGraphComponent) && FirstSelectedIndex != INDEX_NONE)
+	else if(IsValid(GraphComponent) && FirstSelectedIndex != INDEX_NONE)
 	{
-		const FVector& NewLocation = SpatialGraphComponent->GetNodeLocation(FirstSelectedIndex) + DeltaTranslate;
-		const_cast<URpSpatialGraphComponent*>(SpatialGraphComponent)->SetNodeLocation(FirstSelectedIndex, NewLocation);
-		bHandled = true;
+		const FVector& NewLocation = GraphComponent->GetNodeLocation(FirstSelectedIndex) + DeltaTranslate;
+		GraphComponent->SetNodeLocation(FirstSelectedIndex, NewLocation);
 	}
 	
-	return bHandled;
+	return true;
 }
 
 bool FRpGraphVisualizer::HandleInputKey(FEditorViewportClient* ViewportClient, FViewport* Viewport, FKey Key, EInputEvent Event)
@@ -116,37 +105,40 @@ bool FRpGraphVisualizer::HandleInputKey(FEditorViewportClient* ViewportClient, F
 
 	if(ViewportClient->IsAltPressed() && Key == EKeys::X && Event == IE_Released)
 	{
-		if(IsValid(SpatialGraphComponent) && FirstSelectedIndex != INDEX_NONE && SecondSelectedIndex != INDEX_NONE)
+		if(IsValid(GraphComponent) && FirstSelectedIndex != INDEX_NONE && SecondSelectedIndex != INDEX_NONE)
 		{
-			const_cast<URpSpatialGraphComponent*>(SpatialGraphComponent)->DisconnectNodes(FirstSelectedIndex, SecondSelectedIndex);
+			const_cast<URpSpatialGraphComponent*>(GraphComponent)->DisconnectNodes(FirstSelectedIndex, SecondSelectedIndex);
 			SecondSelectedIndex = INDEX_NONE;
 			bHandled = true;
 		}
 	}
 	else if(Key == EKeys::X && Event == IE_Released)
 	{
-		if(IsValid(SpatialGraphComponent) && FirstSelectedIndex != INDEX_NONE && SpatialGraphComponent->GetNumNodes() > 1)
+		if(IsValid(GraphComponent) && FirstSelectedIndex != INDEX_NONE && GraphComponent->GetNumNodes() > 1)
 		{
-			const_cast<URpSpatialGraphComponent*>(SpatialGraphComponent)->DeleteNode(FirstSelectedIndex);
-			FirstSelectedIndex = SpatialGraphComponent->GetNumNodes() != 0 ? SpatialGraphComponent->GetNumNodes() - 1 : INDEX_NONE;
+			const_cast<URpSpatialGraphComponent*>(GraphComponent)->DeleteNode(FirstSelectedIndex);
+			FirstSelectedIndex = GraphComponent->GetNumNodes() != 0 ? GraphComponent->GetNumNodes() - 1 : INDEX_NONE;
 			bHandled = true;
 		}
 	}
-
-	if(ViewportClient->IsCtrlPressed() && Key == EKeys::F && Event == IE_Released)
+	else if(ViewportClient->IsCtrlPressed() && Key == EKeys::F && Event == IE_Released)
 	{
-		if(IsValid(SpatialGraphComponent) && FirstSelectedIndex != INDEX_NONE && SecondSelectedIndex != INDEX_NONE)
+		if(IsValid(GraphComponent) && FirstSelectedIndex != INDEX_NONE && SecondSelectedIndex != INDEX_NONE)
 		{
-			const_cast<URpSpatialGraphComponent*>(SpatialGraphComponent)->ConnectNodes(FirstSelectedIndex, SecondSelectedIndex);
+			const_cast<URpSpatialGraphComponent*>(GraphComponent)->ConnectNodes(FirstSelectedIndex, SecondSelectedIndex);
 			SecondSelectedIndex = INDEX_NONE;
 			bHandled = true;
 		}
 	}
-
-	if(Key == EKeys::LeftMouseButton && Event == IE_Released)
+	else if(Key == EKeys::LeftMouseButton && Event == IE_Released)
 	{
 		bAllowDuplication = true;
 	}
 
 	return bHandled;
+}
+
+UActorComponent* FRpGraphVisualizer::GetEditedComponent() const
+{
+	return GraphComponent;
 }
--- Source/EditorExtensions/Public/RpGraphVisualizer.h
+++ Source/EditorExtensions/Public/RpGraphVisualizer.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "ComponentVisualizer.h"
 
+// Vis Proxies are used to detect clicks (or hits) on visualized elements.
 struct EDITOREXTENSIONS_API HGraphVisProxy : public HComponentVisProxy
 {
 	DECLARE_HIT_PROXY();
@@ -26,30 +27,43 @@ struct EDITOREXTENSIONS_API HNodeVisProxy : public HGraphVisProxy
 	int32 SelectedIndex;
 };
 
+/**
+ * Component Visualizer for Ripple's Spatial Graph Component.
+ *
+ * It represents nodes as points and the connections between them as lines.
+ * The visualizer offers common graph editing features like adding, removing, joining and disjoining nodes.
+ */
 class EDITOREXTENSIONS_API FRpGraphVisualizer : public FComponentVisualizer
 {
 public:
-
-	FRpGraphVisualizer();
 	
 	// Begin FComponentVisualizer interface
 	virtual void DrawVisualization(const UActorComponent* Component, const FSceneView* View, FPrimitiveDrawInterface* PDI) override;
-	
+
+	// Handle clicks for node selection
 	virtual bool VisProxyHandleClick(FEditorViewportClient* InViewportClient, HComponentVisProxy* VisProxy, const FViewportClick& Click) override;
-	
+
+	// Get location of node that is selected currently
 	virtual bool GetWidgetLocation(const FEditorViewportClient* ViewportClient, FVector& OutLocation) const override;
-	
+
+	// Apply translation to selected node
 	virtual bool HandleInputDelta(FEditorViewportClient* ViewportClient, FViewport* Viewport, FVector& DeltaTranslate, FRotator& DeltaRotate, FVector& DeltaScale) override;
 
+	// Consume input for deletion, connection and disconnection commands
 	virtual bool HandleInputKey(FEditorViewportClient* ViewportClient, FViewport* Viewport, FKey Key,EInputEvent Event) override;
+
+	// Get currently edited component
+	virtual UActorComponent* GetEditedComponent() const override;
 
 private:
 
-	const class URpSpatialGraphComponent* SpatialGraphComponent = nullptr;
+	// Currently edited component
+	class URpSpatialGraphComponent* GraphComponent = nullptr;
 	
-	// Index of target in selected component
+	// Index of nodes(s) in currently selected
 	int32 FirstSelectedIndex = INDEX_NONE;
 	int32 SecondSelectedIndex = INDEX_NONE;
 	
+	// Flag that prevents unintended node duplication
 	bool bAllowDuplication = false;
 };
