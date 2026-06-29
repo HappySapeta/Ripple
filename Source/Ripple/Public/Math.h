326c1774cb10278263f0d9bdb999f5091a39b59f
HappySapeta <35849508+HappySapeta@users.noreply.github.com>
2026-06-29 20:45:59 +0100

    math functions

---
+++ Source/Ripple/Public/Math.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+struct FRpMath
+{
+	[[nodiscard]] static UE_FORCEINLINE_HINT float GetSignedAngleDegrees(FVector2f V, FVector2f W)
+	{
+		V.Normalize();
+		W.Normalize();
+		return FMath::RadiansToDegrees(FMath::Atan2((V.X * W.Y) - (V.Y * W.X), (V.X * W.X) + (V.Y * W.Y)));
+	};
+};
+
